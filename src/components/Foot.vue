9ae47b7216876a5c6cf7936f4b380e55f216acdf
home <jianglimin999@gmail.com>
2019-06-12 15:24:30 +0800

    初步完成Foot组件

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -2,20 +2,28 @@
   <div class="bottom-nav">
     <ul>
       <li>
-        <icon name="home"></icon>
-        <div>有赞</div>
+        <a href="#">
+          <icon name="home" class="icon"></icon>
+          <div>有赞</div>
+        </a>
       </li>
       <li>
-        <icon name="category"></icon>
-        <div>分类</div>
+        <a href="#">
+          <icon name="category" class="icon"></icon>
+          <div>分类</div>
+        </a>
       </li>
       <li>
-        <icon name="cart"></icon>
-        <div>购物车</div>
+        <a href="#">
+          <icon name="cart" class="icon"></icon>
+          <div>购物车</div>
+        </a>
       </li>
       <li>
-        <icon name="user"></icon>
-        <div>我</div>
+        <a href="#">
+          <icon name="user" class="icon"></icon>
+          <div>我</div>
+        </a>
       </li>
     </ul>
   </div>
@@ -33,6 +41,39 @@ export default {
 
 <style scoped>
 .bottom-nav {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 50px;
+  border-top: 2px solid #e5e5e5;
+  background-color: #fff;
+}
+.bottom-nav ul {
   display: flex;
+  height: 100%;
+}
+.bottom-nav ul li {
+  flex-basis: 25%;
+}
+.bottom-nav ul li a {
+  display: block;
+  height: 100%;
+  text-decoration: none;
+  color: #666;
+  font-size: 10px;
+  padding-top: 8px;
+  text-align: center;
+}
+
+.bottom-nav ul li a .icon {
+  display: block;
+  width: 22px;
+  height: 22px;
+  margin: auto;
+  margin-bottom: 4px;
+  background-position: 50%;
+  background-repeat: no-repeat;
+  background-size: 100% auto;
 }
 </style>
